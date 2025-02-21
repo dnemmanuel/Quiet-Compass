6fdf43367619128fed70319770510c33a7bd7856
dnemmanuel <dinnelemmanuel@gmail.com>
2025-02-21 16:10:30 -0400

    Line data

---
+++ src/components/lineItem.component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="line-container">
+      <div class="title">Do something</div>
+      <div id="line-status">Hello</div>
+      <div class="dueDate">16/34/25</div>
+      <div class="assignedBy">Kathleen</div>
+      <div class="category">Graphic Design</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
